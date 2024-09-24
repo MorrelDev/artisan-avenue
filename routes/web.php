653db557b8f36b4cba04a6f89cbd9647be1b2c54
morrel <netesov.jezz@gmail.com>
2024-09-24 12:43:48 +0400

    added temporary route to check build files

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
@@ -11,5 +12,16 @@ Route::view('dashboard', 'dashboard')
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
+
+Route::get('/build-check', function () {
+    $files = File::allFiles(public_path('build'));
+
+    $fileList = [];
+    foreach ($files as $file) {
+        $fileList[] = $file->getRelativePathname();
+    }
+
+    return response()->json($fileList);
+});
 
 require __DIR__.'/auth.php';
